5480e20b81efacb57cc8fec1b03a12b249acd63d
cj <jb.carl@outlook.com>
2024-05-30 18:46:57 -0600

    added: Profile Update
    users can update their profile information

--- YumeYomi/users/forms.py
+++ YumeYomi/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +9,23 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+        widgets = {
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+        }
