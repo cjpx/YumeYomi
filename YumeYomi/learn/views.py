e88ee903ad0490a1e3729ab88ea63f093e79d647
cj <jb.carl@outlook.com>
2024-05-31 21:58:58 -0600

    added: Post class

--- YumeYomi/learn/views.py
+++ YumeYomi/learn/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .import views
 from django.http import HttpResponse
+from .models import Post
 from gtts import gTTS
 import io
 
@@ -11,7 +12,9 @@ def home(request):
 
     return render(request, "learn/base.html", context=context)
 
-
+def post(request):
+    context = {'posts': Post.objects.all()}
+    return render(request, "learn/posts.html", context=context)
 
 def generate_audio(request):
     if request.method == 'GET':
