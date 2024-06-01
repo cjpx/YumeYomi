3ee6ec5a67126927717467f1a8db3727e8e7fa67
cj <jb.carl@outlook.com>
2024-05-31 21:52:23 -0600

    added: list urls

--- YumeYomi/YumeYomi/urls.py
+++ YumeYomi/YumeYomi/urls.py
@@ -8,6 +8,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic.base import RedirectView
 from kanas import views as kana_views
 from users import views as user_views
+from users_list import views as users_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,6 +24,17 @@ urlpatterns = [
     path("profile/", user_views.profile, name="profile"),
     path("login/", auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+
+    path('lists/', users_list_view.list_index, name='list_index'),
+    path('list/<int:pk>/', users_list_view.list_detail, name='list_detail'),
+    path('list/new/', users_list_view.create_list, name='create_list'),
+    path('list/<int:list_pk>/add_word/', users_list_view.add_word, name='add_word'),
+    path('tags/new/', users_list_view.add_tag, name='add_tag'),
+    path('list/<int:pk>/toggle_privacy/', users_list_view.toggle_privacy, name='toggle_privacy'),
+    path('like/<int:pk>/', users_list_view.like_word_list, name='like_word_list'),
+    path('unlike/<int:pk>/', users_list_view.unlike_word_list, name='unlike_word_list'),
+    path('add_tag/', users_list_view.add_tag, name='add_tag'),
+    path('word/<int:word_id>/', users_list_view.word_detail, name='word_detail'),
     
     path("", include("learn.urls")),
 ]
