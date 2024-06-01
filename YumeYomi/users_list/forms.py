a509422a0f6305815bf00076c773f58862ceac83
cj <jb.carl@outlook.com>
2024-05-31 21:51:42 -0600

    added: forms

---
+++ YumeYomi/users_list/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Word, WordList, Tag
+
+class WordListForm(forms.ModelForm):
+    class Meta:
+        model = WordList
+        fields = ['title', 'description', 'date_posted']
+    
+
+class WordForm(forms.ModelForm):
+    tags = forms.CharField(max_length=100, required=False, help_text="Enter tags separated by commas")
+
+    class Meta:
+        model = Word
+        fields = ['kanji', 'kana', 'romanji', 'meaning']
+
+    def save(self, commit=True):
+        word = super().save(commit=False)
+        tag_names = self.cleaned_data.get('tags')
+
+        # Split the input tag string into individual tag names
+        tag_names_list = [tag.strip() for tag in tag_names.split(',') if tag.strip()]
+        
+        if commit:
+            word.save()  # Save the word instance first
+            
+            # Create or get existing tags and associate them with the word
+            for tag_name in tag_names_list:
+                tag, _ = Tag.objects.get_or_create(name=tag_name)
+                word.tags.add(tag)
+        
+        return word
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['name']
