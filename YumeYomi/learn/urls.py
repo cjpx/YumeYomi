bdb5078a53e4c74097efd8184c9e0b02990dca48
cj <jb.carl@outlook.com>
2024-07-08 20:52:47 -0600

    added: radical urls

--- YumeYomi/learn/urls.py
+++ YumeYomi/learn/urls.py
@@ -5,6 +5,8 @@ from .import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("posts/", views.post, name="posts"),
+    path("radicals/", views.radicals, name="radicals"),
+    path("radical/<str:radical>", views.radical_detail, name="radical_detail"),
 
     path('generate_audio/', views.generate_audio, name='generate_audio'),
 ]
