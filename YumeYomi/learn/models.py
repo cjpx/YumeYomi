5e1bb0cd964b972a28efa41b1d1a09c9c0f015cf
cj <jb.carl@outlook.com>
2024-07-08 20:52:11 -0600

    added: radical model

--- YumeYomi/learn/models.py
+++ YumeYomi/learn/models.py
@@ -12,3 +12,21 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class Radicals(models.Model):
+    radical_id = models.PositiveIntegerField(primary_key=True)
+    radical = models.CharField(max_length=1, unique=True, verbose_name="Radical Character")
+    other_forms = models.CharField(max_length=50, blank=True, verbose_name="Other Forms")
+    radical_meaning = models.CharField(max_length=100, verbose_name="Meaning")
+    radical_stroke_count = models.PositiveIntegerField(verbose_name="Stroke Count")
+    reading_hiragana = models.CharField(max_length=100, blank=False, default='', verbose_name="Hiragana Reading")
+    reading_romanji = models.CharField(max_length=100, blank=False, default='', verbose_name="Romanji Reading")
+
+    class Meta:
+        verbose_name = "Radical"
+        verbose_name_plural = "Radicals"
+        ordering = ['radical_id']
+
+    def __str__(self):
+        return f"{self.radical} ({self.radical_meaning})"
+
