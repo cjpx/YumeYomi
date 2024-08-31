659f26b09519e12789414f52edec4d9b6bef1dc7
cj <jb.carl@outlook.com>
2024-08-30 22:32:32 -0600

    added: character selection and radical_selection, fetch_characters, and character_detail  views

--- YumeYomi/learn/views.py
+++ YumeYomi/learn/views.py
@@ -1,19 +1,54 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
 from .import views
-from .forms import RadicalForm
-from django.http import HttpResponse
-from .models import Post, Radicals
+from django.http import HttpResponse, JsonResponse
+from .models import Post, Radical, Character, CharacterEdit
+from django.contrib.auth.decorators import login_required
+from .forms import CharacterEditForm
+from django.db.models import Q
 from gtts import gTTS
 import io
 
 
 
+def fetch_characters(request):
+    selected_radicals = request.GET.getlist('radicals[]')  # Get list of selected radicals' IDs
+    if not selected_radicals:
+        return JsonResponse({'characters': []})
+
+    try:
+        radical_ids = [int(id) for id in selected_radicals]
+        radicals = Radical.objects.filter(radical_id__in=radical_ids)
+        characters = Character.objects.filter(radicals__in=radicals).distinct()
+        # Include 'id' in the character data
+        character_data = list(characters.values('id', 'name', 'meaning', 'readings'))
+        return JsonResponse({'characters': character_data})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+    
+
+def character_detail(request, pk):
+    character = get_object_or_404(Character, pk=pk)
+    return render(request, 'learn/character_detail.html', {
+        'character': character
+    })
+
+
+def radical_selection(request):
+    radicals = Radical.objects.all()
+    return render(request, 'learn/radical_selection.html', {
+        'radicals': radicals
+    })
+    
+
+
+
 def radicals(request):
-    radicals = Radicals.objects.all()
+    radicals = Radical.objects.all()
     return render(request, "learn/radicals.html", {'radicals': radicals})
 
 def radical_detail(request, radical):
-    radical_obj = get_object_or_404(Radicals, radical=radical)
+    radical_obj = get_object_or_404(Radical, radical=radical)
     readings_hiragana = radical_obj.reading_hiragana.split(", ")
     readings_romanji = radical_obj.reading_romanji.split(", ")
 
