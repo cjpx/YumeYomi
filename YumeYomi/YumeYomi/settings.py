82417fcfa56ffab85d259a816ffce4b9abf0e4bc
cj <jb.carl@outlook.com>
2024-08-30 22:39:42 -0600

    allwed all host

--- YumeYomi/YumeYomi/settings.py
+++ YumeYomi/YumeYomi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yjtbe1y5j%i-oe)#-)b&d!a0hnimb*lg!ac)*l%+*w*5jwz4u+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
