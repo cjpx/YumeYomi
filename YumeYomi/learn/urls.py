c980b7ed02afd795dc966b51e897659a059b9da2
cj <jb.carl@outlook.com>
2024-12-12 13:50:14 -0700

    add:url patter
    added url pattern to fetch and search character by name

--- YumeYomi/learn/urls.py
+++ YumeYomi/learn/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path("radicals/", views.radicals, name="radicals"),
     path("radical/<str:radical>", views.radical_detail, name="radical_detail"),
 
+    path('radicals/search/', views.radical_selection, name='radical_selection'),
+    path('fetch-characters/', views.fetch_characters, name='fetch_characters'),
+
+    path('character/<str:name>/', views.character_detail, name='character_detail'),
+    #path('character/<int:pk>/edit/', views.submit_edit, name='submit_edit'),
+
     path('generate_audio/', views.generate_audio, name='generate_audio'),
 ]
