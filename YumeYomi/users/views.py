ad768c5667fcf3147742ef1ffc706a7b796e697a
cj <jb.carl@outlook.com>
2025-02-10 23:31:16 -0700

    feat redesign profile function to enable user's to view others profile

--- YumeYomi/users/views.py
+++ YumeYomi/users/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .models import User, Profile
 
 def register(request):
     if request.method == "POST":
@@ -17,23 +18,28 @@ def register(request):
     return render(request, "users/register.html", {'form': form})
 
 @login_required
-def profile(request):
+def profile(request, username):
+    user = get_object_or_404(User, username=username)  # Fetch user by username
+    profile = get_object_or_404(Profile, user=user)  # Get user's profile
+
     if request.method == "POST":
-        u_form = UserUpdateForm(request.POST, instance=request.user)  # user update form
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)  # profile update form
+        u_form = UserUpdateForm(request.POST, instance=user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
 
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile')
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile', username=user.username)  # Redirect to updated profile
+
     else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+        u_form = UserUpdateForm(instance=user)
+        p_form = ProfileUpdateForm(instance=profile)
 
     context = {
+        'profile': profile,
         'u_form': u_form,
-        'p_form': p_form
+        'p_form': p_form,
     }
 
     return render(request, "users/profile.html", context)
